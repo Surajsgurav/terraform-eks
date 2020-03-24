055f08faeaac6adfe0f7329e0c81238393ca10b9
sushmitabhandari <42686549+sushmitabhandari@users.noreply.github.com>
2020-03-24 18:24:55 +0530

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -7,7 +7,7 @@
 resource "aws_security_group" "eks-cluster" {
   name        = var.cluster_defaults["eks_securitygrp"]
   description = "Cluster communication with worker nodes"
-  vpc_id      = "${aws_vpc.vpc.id}"
+  vpc_id      = aws_vpc.vpc.id
 
   egress {
     from_port   = 0
@@ -26,8 +26,8 @@ resource "aws_security_group_rule" "cluster-ingress-node-https" {
   description              = "Allow pods to communicate with the cluster API Server"
   from_port                = 443
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.eks-cluster.id}"
-  source_security_group_id = "${aws_security_group.eks-nodes.id}"
+  security_group_id        = aws_security_group.eks-cluster.id
+  source_security_group_id = aws_security_group.eks-nodes.id
   to_port                  = 443
   type                     = "ingress"
 }
@@ -37,21 +37,21 @@ resource "aws_security_group_rule" "cluster-ingress-laptop-https" {
   description       = "Allow laptop to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.eks-cluster.id}"
+  security_group_id = aws_security_group.eks-cluster.id
   to_port           = 443
   type              = "ingress"
 }
 
 resource "aws_eks_cluster" "eks-cluster" {
   name     = var.cluster_defaults["eks_cluster"]
-  role_arn = "${aws_iam_role.eks-cluster.arn}"
+  role_arn = aws_iam_role.eks-cluster.arn
 
   vpc_config {
     security_group_ids = ["${aws_security_group.eks-cluster.id}"]
 
     subnet_ids = [
-      "${aws_subnet.subnet-1a-prv.*.id}",
-      "${aws_subnet.subnet-1c-prv.*.id}",
+      aws_subnet.subnet-1a-prv.*.id,
+      aws_subnet.subnet-1c-prv.*.id,
     ]
   }
 
@@ -64,15 +64,15 @@ resource "aws_eks_cluster" "eks-cluster" {
 resource "aws_iam_role" "eks-cluster" {
   name               = var.cluster_defaults["eks_iam"]
   path               = "/"
-  assume_role_policy = "${file("./json/cluster-role-policy.json")}"
+  assume_role_policy = file("./json/cluster-role-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "k8s-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.eks-cluster.name}"
+  role       = aws_iam_role.eks-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "k8s-cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.eks-cluster.name}"
+  role       = aws_iam_role.eks-cluster.name
 }
